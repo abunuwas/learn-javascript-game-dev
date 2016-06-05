a299e0b48db1d21da0eef7d0fa9121ab70d9cae8
abunuwas <joseharoperalta@gmail.com>
2016-06-05 22:16:18 +0100

    Add bricks to the game

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+'use strict';
+
+// Canvas variables
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
 
+// Ball variables 
 var x = canvas.width/2;
 var y = canvas.height-30;
 
@@ -11,13 +15,33 @@ var ballRadius = 10;
 
 var color = '#0095DD';
 
+// Paddle variables 
 var paddleHeight = 10;
-var paddleWidth = 75;
+var paddleWidth = 100;
 var paddleX = (canvas.width-paddleWidth)/2;
 
+// Keyboard variables 
 var rightPressed = false;
 var leftPressed = false;
 
+// Bricks variables
+var brickRowCount = 3;
+var brickColumnCount = 8;
+var brickWidth = 100;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 40;
+var brickOffsetLeft = 57;
+
+var bricks = [];
+for(var c = 0; c < brickColumnCount; c++) {
+	bricks[c] = [];
+	for(var r=0; r < brickRowCount; r++) {
+		bricks[c][r] = { x: 0, y: 0 };
+	};
+};
+
+// Register keyboard event listeners
 document.addEventListener(
 						  'keydown', 
 						   keyDownHandler, 
@@ -27,6 +51,7 @@ document.addEventListener(
 						  keyUpHandler, 
 						  false);
 
+// Functions 
 function keyDownHandler(e) {
 	if(e.keyCode == 39) {
 		rightPressed = true;
@@ -64,11 +89,28 @@ function drawPaddle() {
 	ctx.fillStyle = '#0995DD';
 	ctx.fill();
 	ctx.closePath();
+};
+
+function drawBricks() {
+	for(c=0; c < brickColumnCount; c++) {
+		for(r=0; r < brickRowCount; r++) {
+			var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+			var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+			bricks[c][r].x = brickX;
+			bricks[c][r].y = brickY;
+			ctx.beginPath();
+			ctx.rect(brickX, brickY, brickWidth, brickHeight);
+			ctx.fillStyle = '#0095DD';
+			ctx.fill();
+			ctx.closePath();
+		};
+	};
 }
 
 function draw() {
 	// drawing code
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	drawBricks();
 	drawBall();
 	drawPaddle();
 	if(y + dy < ballRadius) {
